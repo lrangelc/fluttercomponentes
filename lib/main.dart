9dcf1db7d68c4a4f698e3fc6f5ea78ffef4e0d1b
Luis Rangel <luisrangelc@rpaipos.com>
2020-02-01 20:17:21 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/pages/home_temp.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp(),
+    );
+  }
+}
---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = [
+    'uno',
+    'due',
+    'tre',
+    'quattro',
+    'cinque',
+    'sei',
+    'sette',
+    'otto',
+    'nove',
+    'dieci'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(children: _crearItems()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (var item in opciones) {
+      final tempWidget = ListTile(
+          title: Text('$item'),
+        );
+
+      lista
+        ..add(tempWidget)
+        ..add(Divider(
+          color: Colors.black,
+        ));
+    }
+    return lista;
+  }
+}
