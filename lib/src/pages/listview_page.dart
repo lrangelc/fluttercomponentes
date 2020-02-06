3130306b44012d61152a78dcda128884c2316887
Luis Rangel <luisrangelc@rpaipos.com>
2020-02-05 22:45:14 -0600

    pull to refresh

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,4 +1,8 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+
+import 'dart:async';
 
 class ListaPage extends StatefulWidget {
   ListaPage({Key key}) : super(key: key);
@@ -11,9 +15,11 @@ class _ListaPageState extends State<ListaPage> {
   ScrollController _scrollController = new ScrollController();
   List<int> _listaNumeros = new List();
   int _ultimoItem = 0;
+  bool _isLoading = false;
 
   @override
   void initState() {
+    super.initState();
     _agregar10();
     _scrollController.addListener(cambioScroll);
     // _scrollController.addListener(() {
@@ -23,15 +29,21 @@ class _ListaPageState extends State<ListaPage> {
     //     _agregar10();
     //   }
     // });
+  }
 
-    super.initState();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
   }
 
   void cambioScroll() {
     // print('Scrol2!!');
     if (_scrollController.position.pixels ==
         _scrollController.position.maxScrollExtent) {
-      _agregar10();
+      // _agregar10();
+      fetchData();
     }
   }
 
@@ -41,25 +53,62 @@ class _ListaPageState extends State<ListaPage> {
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: <Widget>[_crearLista(), _crearLoading()],
+      ),
     );
   }
 
-  Widget _crearLista() {
-    return ListView.builder(
-      controller: _scrollController,
-      itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext context, int indice) {
-        final image = _listaNumeros[indice];
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CircularProgressIndicator()],
+          ),
+          SizedBox(
+            height: 15.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
 
-        return FadeInImage(
-          placeholder: AssetImage('assets/image_loading.gif'),
-          fadeInDuration: Duration(milliseconds: 200),
-          fit: BoxFit.contain,
-          image: NetworkImage('https://i.picsum.photos/id/$image/500/300.jpg'),
-        );
-      },
+  Widget _crearLista() {
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int indice) {
+          final image = _listaNumeros[indice];
+
+          return FadeInImage(
+            placeholder: AssetImage('assets/image_loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            fit: BoxFit.contain,
+            image:
+                NetworkImage('https://i.picsum.photos/id/$image/500/300.jpg'),
+          );
+        },
+      ),
     );
+  }
+
+  Future<Null> obtenerPagina1() async {
+    final duracion = new Duration(seconds: 2);
+    new Timer(duracion, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duracion);
   }
 
   void _agregar10() {
@@ -69,5 +118,19 @@ class _ListaPageState extends State<ListaPage> {
     }
 
     setState(() {});
+  }
+
+  Future<Null> fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duracion = new Duration(seconds: 2);
+    return new Timer(duracion, respuestaHTTP);
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 200,
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _agregar10();
   }
 }
